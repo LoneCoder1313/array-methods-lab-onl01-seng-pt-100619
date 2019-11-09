337515424f5b136e3b39955171199e91c25b4b25
Ricky dildy <rdildy92@gmail.com>
2019-11-09 00:43:05 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,31 +1,36 @@
 def using_include(array, element)
-  array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  element1 = "wow"
-  element2 = "tardis"
-  expect(using_include(array, element1)).to eq(true)
-  expect(using_include(array, element2)).to eq(false)
-end
+      array = ["wow", "I", "am", "really", "learning", "arrays!"]
+          element1 = "wow"
+              element2 = "Tardis"
+               expect(using_include(array, element1)).to eq(true)   
+                   expect(using_include(array, element2)).to eq(false)
+                 end 
 
 def using_sort(array)
-  array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  expect(using_sort(array).last).to eq("wow")
-  expect(using_sort(sort).first).to eq("I")
-end 
-
+    array = ["wow", "I", "am", "really", "learning", "arrays!"]
+    expect(using_sort(array).last).to eq("wow")
+    expect(using_sort(array).first).to eq("I")
+  end 
+  
 def using_reverse(array)
-  array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  expect(using_reverse(array).first).to eq("arrays!")
-  expect(using_reverse(array).last).to eq("wow")
-end
-
+    array = ["wow", "I", "am", "really", "learning", "arrays!"]
+    expect(using_reverse(array).first).to eq("arrays!")
+    expect(using_reverse(array).last).to eq("wow")
+  end 
+  
 def using_first(array)
-
-end
-
+    array = ["wow", "I", "am", "really", "learning", "arrays!"]
+    expect(using_first(array)).to eq("wow")
+  end 
+  
 def using_last(array)
-
-end
-
+    array = ["wow", "I", "am", "really", "learning", "arrays!"]
+    expect(using_last(array)).to eq("arrays!")
+  end 
+  
 def using_size(array)
+    array = ["wow", "I", "am", "really", "learning", "arrays!"]
+    expect(using_size(array)).to eq(6)
+  end 
+  
 
-end
